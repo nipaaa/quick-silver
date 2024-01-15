8dd4c12e113d6ff191241d06a4c08b293b450e46
nipaaa <akternipa941@gmail.com>
2024-01-15 18:45:35 +0600

    forget password

---
+++ src/pages/forgot-password.js
@@ -0,0 +1,12 @@
+import Forgot from "@/Components/Auth/Forgot";
+import React from "react";
+
+const ForgetPassword = () => {
+  return (
+    <div>
+      <Forgot />
+    </div>
+  );
+};
+
+export default ForgetPassword;
