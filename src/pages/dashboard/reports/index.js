03e1e31e2b1ab915f84da2fe959c0981886b0dbf
mohi14 <mohiuddinkhan1409@gmail.com>
2024-01-18 10:07:34 +0600

    some changes

---
+++ src/pages/dashboard/reports/index.js
@@ -0,0 +1,12 @@
+import DashboardLayout from "@/Layout/DashboardLayout";
+import React from "react";
+
+const Reports = () => {
+  return (
+    <DashboardLayout>
+      <div>jfksf</div>
+    </DashboardLayout>
+  );
+};
+
+export default Reports;
