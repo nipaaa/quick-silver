efc0e4d98c48e117f767c8ca1c1de5bd29504c45
nipaaa <akternipa941@gmail.com>
2024-01-20 18:05:48 +0600

    report step 1 add

--- src/pages/dashboard/reports/index.js
+++ src/pages/dashboard/reports/index.js
@@ -1,10 +1,14 @@
+import StepOne from "@/Components/Reports/StepOne";
 import DashboardLayout from "@/Layout/DashboardLayout";
-import React from "react";
+import React, { useState } from "react";
 
 const Reports = () => {
+  const [stepper, setStepper] = useState(1);
   return (
     <DashboardLayout>
-      <div>jfksf</div>
+      <div className="m-4 lg:mx-[70px] lg:my-[50px]">
+        {stepper === 1 && <StepOne />}
+      </div>
     </DashboardLayout>
   );
 };
