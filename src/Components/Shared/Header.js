748bf14c4bd3dbd0d306abb64e556c123523acca
nipaaa <akternipa941@gmail.com>
2024-01-25 17:52:26 +0600

    header title dynamic

--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { IoReorderThreeOutline } from "react-icons/io5";
 import dot from "../../assets/dot.png";
 import arrow from "../../assets/green-arrow.png";
@@ -8,18 +8,53 @@ import profile from "../../assets/profile.png";
 import signOut from "../../assets/sign-out.png";
 import CustomModal from "./Modal/CustomModal";
 import Profile from "./Modal/Profile";
+import { useRouter } from "next/router";
 
 const Header = ({ handleSidebar }) => {
+  const profileRef = useRef();
+  const router = useRouter();
+  const { pathname } = router;
   const [show, setShow] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
 
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (profileRef.current && !profileRef.current.contains(event.target)) {
+        setShow(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [profileRef]);
+
   return (
     <div>
       <div className="header flex md:flex-none flex-wrap md:flex-nowrap gap-4 md:gap-0">
         <div className="header_title flex items-center flex-wrap gap-4">
-          <h2 className="text-[28px] font-bold">Office Dashboard</h2>
+          <h2 className="text-[28px] font-bold">
+            {pathname === "/dashboard"
+              ? "Office Dashboard"
+              : pathname === "/dashboard/inspection/search"
+              ? "Search"
+              : pathname === "/dashboard/edit-inspection"
+              ? "Edit Inspection"
+              : pathname === "/dashboard/new-inspection"
+              ? "New Inspection"
+              : pathname === "/dashboard/administration/entities"
+              ? " Entities"
+              : pathname === "/dashboard/administration/company"
+              ? "Company"
+              : pathname === "/dashboard/administration/lists"
+              ? "Lists"
+              : pathname === "/dashboard/reports"
+              ? "Reports"
+              : ""}
+          </h2>
           <button onClick={handleSidebar} className="block md:hidden">
             <IoReorderThreeOutline className="text-xl font-bold" />
           </button>
@@ -28,7 +63,8 @@ const Header = ({ handleSidebar }) => {
           <input type="search" placeholder="Search" />
           <div
             onClick={() => setShow(!show)}
-            className="flex items-center gap-2 w-full profile cursor-pointer ">
+            className="flex items-center gap-2 w-full profile cursor-pointer "
+          >
             <img
               height={40}
               width={40}
@@ -41,7 +77,10 @@ const Header = ({ handleSidebar }) => {
           </div>
 
           {show && (
-            <div className="flex items-start absolute gap-4 justify-between profile_box top-[110px] md:top-[50px] right-[0px] md:right-[30px] md:width-[496px] height-[100%] fs_18 z-10">
+            <div
+              ref={profileRef}
+              className="flex items-start absolute gap-4 justify-between profile_box top-[110px] md:top-[50px] right-[0px] md:right-[30px] md:width-[496px] height-[100%] fs_18 z-10"
+            >
               <div className="flex flex-wrap items-start gap-4">
                 <img
                   height={59}
@@ -58,14 +97,16 @@ const Header = ({ handleSidebar }) => {
                   <Link
                     style={{ display: "flex" }}
                     className="  items-center gap-3 "
-                    href="#">
+                    href="#"
+                  >
                     <img src={manage.src} alt="" />
                     Manage account
                   </Link>
                   <Link
                     style={{ display: "flex" }}
                     className="  items-center gap-3"
-                    href="#">
+                    href="#"
+                  >
                     <img src={signOut.src} alt="" />
                     Sign Out
                   </Link>
@@ -80,7 +121,8 @@ const Header = ({ handleSidebar }) => {
         isOpen={modalOpen}
         onClose={closeModal}
         submitTitle={"Save"}
-        onSave={closeModal}>
+        onSave={closeModal}
+      >
         <Profile />
       </CustomModal>
     </div>
