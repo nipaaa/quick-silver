9ebc84f05cf0448dfbb6b3e1fd0d65f46e50ad2a
nipaaa <akternipa941@gmail.com>
2024-01-27 15:34:19 +0600

    update

--- src/Components/Auth/SignIn.js
+++ src/Components/Auth/SignIn.js
@@ -3,9 +3,44 @@ import Link from "next/link";
 import { useState } from "react";
 import eyeOff from "../../assets/eye-off.png";
 import eye from "../../assets/eye.png";
+import { useRouter } from "next/router";
 
 const SignIn = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const { push } = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    setErrorMessage("");
+
+    if (!validateEmail(email)) {
+      setErrorMessage("Invalid email address");
+      return;
+    }
+
+    if (password.length < 8) {
+      setErrorMessage("Password must be at least 8 characters");
+      return;
+    }
+
+    const data = {
+      email,
+      password,
+    };
+    console.log(data);
+
+    push("/dashboard");
+  };
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
   return (
     <div className="signIn">
       <div className="flex flex-wrap justify-end gap-2 items-center">
@@ -14,7 +49,7 @@ const SignIn = () => {
           Sign Up
         </Link>
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2 className="text-[32px]">Sign In</h2>
         <p className="fs-6 subtitle">To continue to the system</p>
         <div className="mb-4">
@@ -55,15 +90,14 @@ const SignIn = () => {
             />
           </div>
           <div className="flex flex-wrap justify-between mb-5 gap-5 items-center">
-            <p className="err_sms">Invalid Username or Password</p>
+            <p className="err_sms">{errorMessage}</p>
             <Link href="/forgot-password" className="forgot fs_14">
               Forgot password?
             </Link>
           </div>
         </div>
-        <Link href="/dashboard">
-          <button className="signIn_btn text-[20px]">Sign In</button>
-        </Link>
+
+        <button className="signIn_btn text-[20px]">Sign In</button>
       </form>
       <div></div>
     </div>
